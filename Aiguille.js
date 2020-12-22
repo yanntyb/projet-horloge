8437cefa16dcc06e1e979a00d9a7f03df42025c4
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-22 15:27:11 +0100

    srdft

--- Aiguille.js
+++ Aiguille.js
@@ -1,7 +1,16 @@
+let date = new Date();
+console.log(date.getMinutes())
+
 let Aiguille = function (taille,couleur,parent){
     this.taille = taille;
     this.couleur = couleur;
-    this.deg = 0;
+    if(this.taille === 250){
+        this.deg = date.getSeconds() * 6;
+    }else if(this.taille === 200){
+        this.deg = date.getMinutes() * 6;
+    }else if(this.taille === 150){
+        this.deg = date.getHours() * 6;
+    }
     this.parent = parent.div;
     this.div = document.createElement("div");
     this.div.style.height = this.taille + "px";
@@ -11,6 +20,8 @@ let Aiguille = function (taille,couleur,parent){
     this.div.style.top = 300 - this.taille + "px";
     this.div.style.border = "1px solid " + this.couleur;
     this.div.className = "aiguille";
+    this.div.style.transformOrigin = "bottom";
+    this.div.style.transform = "rotate(" + this.deg + "deg)";
 }
 
 Aiguille.prototype.afficher = function (){
